206dabd16c080b2050c688d01beacf3298ad98f1
Amador Flores <amador.flores@codebay.us>
2026-09-16 20:45:00 -0600

    prrety code

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/gym-3-dias/',
-})
+  base: "/gym-3-dias/",
+});
